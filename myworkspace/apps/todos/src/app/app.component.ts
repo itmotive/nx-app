db48e5d528166dbd2d80c07fe80e5a5aa699b4d7
Przemysław <przemyslaw.obrzut@gmail.com>
2019-11-27 00:39:19 +0100

    feat(api): add Nest and basic api

--- myworkspace/apps/todos/src/app/app.component.ts
+++ myworkspace/apps/todos/src/app/app.component.ts
@@ -1,4 +1,9 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+interface Todo {
+  title: string;
+}
 
 @Component({
   selector: 'myworkspace-root',
@@ -7,4 +12,19 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'todos';
+  todos: Todo[] = [];
+
+  constructor(private http: HttpClient) {
+    this.fetch();
+  }
+
+  fetch() {
+    this.http.get<Todo[]>('/api/todos').subscribe(t => (this.todos = t));
+  }
+
+  addTodo() {
+    this.http.post('/api/addTodo', {}).subscribe(() => {
+      this.fetch();
+    });
+  }
 }
